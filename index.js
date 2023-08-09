03699840b8a05cfd2911e3e3ad76d2aaba58ecec
Sagar <dev.sagartmg@gmail.com>
2023-08-09 16:01:50 +0545

    variables

---
+++ index.js
@@ -0,0 +1,82 @@
+// console.log("Hello")
+// console.log(1212)
+// console.log(true)
+// console.log(false)
+// console.log('Hello')
+
+/* data types
+    text -> string
+    number -> integer, float/dobule
+    true/false -> boolean 
+*/
+
+// console.log("Hello ram")
+// console.log("Hello shyam")
+// console.log("Hello hari")
+
+/* DRY -> donot repeat yourself.  */
+
+/* variable  // box/ container
+    <key-word> = <value>
+*/
+let greeting = "Namaste"
+
+console.log(greeting, "ram");
+console.log(greeting, "shyam");
+console.log(greeting, "hari");
+
+// age = 30
+let age = "thirty"
+
+console.log("Age is ", age)
+
+
+let married = false
+console.log("marriage status is ", married)
+
+/* naming convention */
+let firstname = "john" // xx wrong way 
+let first_name = "john"  // snake_case
+let firstName = "john" //  camelCase
+
+let lastName = "doe"
+
+console.log(firstname)
+console.log(first_name)
+console.log(firstName)
+
+
+let fullName = firstName + lastName
+
+console.log("fullname is", fullName)
+
+let number1 = 100
+let number2 = 200
+let number3 = number1 + number2
+
+console.log(number3)
+
+
+let brand1 = "apple inc"
+let brand2 = "lg"
+
+console.log(brand1)
+console.log(brand2)
+
+
+/* 
+    h1{
+        color:red;
+        color:orange
+    }
+
+*/
+
+
+
+let count = 100
+count = 200  // re-initialization
+console.log("count",count)
+
+/* let vs const vs var */
+/* declaration vs initialization */
