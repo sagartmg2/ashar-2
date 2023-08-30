d818130d8e9df867a69fd1dbcd48b0a29cb29b8f
Sagar <dev.sagartmg@gmail.com>
2023-08-30 16:18:57 +0545

    dom manipulation

--- filterMethod.js
+++ filterMethod.js
@@ -1,5 +1,32 @@
 let numbers = [2, 1, 4, 3, 5, 6, 7]
-let evenNumbers = []
+
+
+const checkEvenNumber1 = (input) => {
+    if (input % 2 === 0) {
+        return true;
+    }
+    return false;
+
+    /* ternary operator */
+
+}
+const checkEvenNumber = (input) => {
+    return input % 2 === 0
+    return input % 2 == 0 ? true : false
+
+}
+
+console.log(checkEvenNumber(1))
+console.log(checkEvenNumber(4))
+
+// let evenNumbers = numbers.filter(checkEvenNumber)
+let evenNumbers1 = numbers.filter((number) => {return number % 2 === 0})
+let evenNumbers = numbers.filter((number) => number % 2 === 0)
+
+// console.log({ evenNumbers });
+
+
+
 
 let oddNumbers = numbers.filter(el => {
     // console.log("filtering")
@@ -9,8 +36,8 @@ let oddNumbers = numbers.filter(el => {
      !true // false
      !false // true
     */
-   
-    if (el % 2 !== 0) {     
+
+    if (el % 2 !== 0) {
         return true
     }
 })
